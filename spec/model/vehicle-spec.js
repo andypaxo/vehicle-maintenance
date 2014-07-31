69ba5b492716a055383f2b549826b0891270bacd
andypaxo <andy@andypaxo.net>
2014-07-30 19:22:06 -0600

    Add some basic functionality to Vehicle

--- spec/model/vehicle-spec.js
+++ spec/model/vehicle-spec.js
@@ -5,4 +5,28 @@ describe("Vehicle", function () {
 		expect(sut.model).toBe("Mondeo");
 		expect(sut.year).toBe(1998);
 	});
+
+	it("can have the odometer updated", function () {
+		var sut = new Vehicle();
+		expect(sut.getOdometer()).toBe(0);
+		sut.setOdometer(10538);
+		expect(sut.getOdometer()).toBe(10538);
+	});
+
+	it("cannot have the odometer dialled back", function () {
+		var sut = new Vehicle();
+		sut.setOdometer(10538);
+		expect(function () {
+			sut.setOdometer(1024);
+		}).toThrow();
+		expect(sut.getOdometer()).toBe(10538);
+	});
+
+	it("must have the odometer set to a numeric value", function () {
+		var sut = new Vehicle();
+		expect(function () {
+			sut.setOdometer("One hundred and eighty");
+		}).toThrow();
+		expect(sut.getOdometer()).toBe(0);
+	});
 });
--- src/model/vehicle.js
+++ src/model/vehicle.js
@@ -2,4 +2,15 @@ Vehicle = function(make, model, year) {
 	this.make = make;
 	this.model = model;
 	this.year = year;
+	this.odometer = 0;
+
+	this.setOdometer = function (value) {
+		if (isNaN(value) || value < this.odometer)
+			throw new Error("Odometer must be set to at least " + this.odometer);
+		this.odometer = value;
+	};
+
+	this.getOdometer = function () {
+		return this.odometer;
+	};
 };
